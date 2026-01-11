bcaf49ee742fe46df69bd0113f5a1b424c77eee8
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2026-01-11 20:10:15 +0900

    Create Infrared-Obstacle-Avoidance.py

---
+++ Pico 2 w/pico2go/Infrared Obstacle Avoidance/Infrared-Obstacle-Avoidance.py
@@ -0,0 +1,24 @@
+import utime
+from Motor import PicoGo
+from machine import Pin
+
+M = PicoGo()
+DSR = Pin(2, Pin.IN)
+DSL = Pin(3, Pin.IN)
+
+while True:
+    DR_status = DSR.value()
+    DL_status = DSL.value()
+
+    if((DL_status == 0) and (DR_status == 0)):
+        M.left(10)
+    elif((DL_status == 0) and (DR_status == 1)):
+        M.right(10)
+    elif((DL_status == 1) and (DR_status == 0)):
+        M.left(10)
+    else:
+        M.forward(20)
+        
+    print(f'DR:{DR_status}, DL:{DL_status}')    
+    utime.sleep_ms(10)
+
