4e6823a743a2e46b0d4f3333c2b56f6bc31cfae1
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2026-01-11 16:25:53 +0900

    Update ST7789.py

--- Pico 2 w/pico2go/Battery Voltage Detection/ST7789.py
+++ Pico 2 w/pico2go/Battery Voltage Detection/ST7789.py
@@ -1,39 +1,161 @@
-from machine import Pin
-from ST7789 import ST7789
-import utime
+from machine import Pin,SPI
+import framebuf
+import time
 
-bat = machine.ADC(Pin(26))
-temp = machine.ADC(4)
+class ST7789(framebuf.FrameBuffer):
+    def __init__(self):
+        self.width = 240
+        self.height = 135
+        
+        self.rst = Pin(12,Pin.OUT)
+        self.bl = Pin(13,Pin.OUT)
+        self.bl(1)
+        
+        self.cs = Pin(9,Pin.OUT)
+        self.cs(1)
+        self.spi = SPI(1)
+        self.spi = SPI(1,1000_000)
+        self.spi = SPI(1,10000_000,polarity=0, phase=0,sck=Pin(10),mosi=Pin(11),miso=None)
+        self.dc = Pin(8,Pin.OUT)
+        self.dc(1)
+        self.buffer = bytearray(self.height * self.width * 2)
+        super().__init__(self.buffer, self.width, self.height, framebuf.RGB565)
+        self.init_display()
+        
+        self.WHITE  =   0xFFFF
+        self.BLACK  =  0x0000
+        self.GREEN  =  0x001F
+        self.RED    =  0xF800
+        self.BLUE   = 0xFF00
+        self.GBLUE = 0X07FF
+        self.YELLOW = 0xFFE0
+        
+    def write_cmd(self, cmd):
+        self.cs(1)
+        self.dc(0)
+        self.cs(0)
+        self.spi.write(bytearray([cmd]))
+        self.cs(1)
 
-lcd = ST7789()
-lcd.fill(0xF232)
-lcd.line(2,2,70,2,0xBB56)
-lcd.line(70,2,85,17,0xBB56)
-lcd.line(85,17,222,17,0xBB56)
-lcd.line(222,17,237,32,0xBB56)
-lcd.line(2,2,2,118,0xBB56)
-lcd.line(2,118,17,132,0xBB56)
-lcd.line(17,132,237,132,0xBB56)
-lcd.line(237,32,237,132,0xBB56)
+    def write_data(self, buf):
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(bytearray([buf]))
+        self.cs(1)
 
-lcd.text("Raspberry Pi Pico",90,7,0xFF00)
-lcd.text("PicoGo",10,7,0x001F)
-lcd.text("Waveshare.com",70,120,0x07E0)
-lcd.show()
+    def init_display(self):
+        """Initialize dispaly"""
+        self.rst(1)
+        self.rst(0)
+        self.rst(1)
+        
+        self.write_cmd(0x36)
+        self.write_data(0x70)
 
-while (1):
-    utime.sleep(1)
-    reading = temp.read_u16() * 3.3 / (65535)
-    temperature = 27 - (reading - 0.706)/0.001721
-    v = bat.read_u16()*3.3/65535 * 2
-    p = (v - 3) * 100 / 1.2
-    if(p < 0):p=0
-    if(p > 100):p=100
+        self.write_cmd(0x3A) 
+        self.write_data(0x05)
 
-    lcd.fill_rect(145,50,65,40,0xF232)
-    lcd.text("temperature :  {:5.2f} C".format(temperature),30,50,0xFFFF)
-    lcd.text("Voltage     :  {:5.2f} V".format(v),30,65,0xFFFF)
-    lcd.text("percent     :   {:3.1f} %".format(p),30,80,0xFFFF)
+        self.write_cmd(0xB2)
+        self.write_data(0x0C)
+        self.write_data(0x0C)
+        self.write_data(0x00)
+        self.write_data(0x33)
+        self.write_data(0x33)
 
+        self.write_cmd(0xB7)
+        self.write_data(0x35) 
+
+        self.write_cmd(0xBB)
+        self.write_data(0x19)
+
+        self.write_cmd(0xC0)
+        self.write_data(0x2C)
+
+        self.write_cmd(0xC2)
+        self.write_data(0x01)
+
+        self.write_cmd(0xC3)
+        self.write_data(0x12)   
+
+        self.write_cmd(0xC4)
+        self.write_data(0x20)
+
+        self.write_cmd(0xC6)
+        self.write_data(0x0F) 
+
+        self.write_cmd(0xD0)
+        self.write_data(0xA4)
+        self.write_data(0xA1)
+
+        self.write_cmd(0xE0)
+        self.write_data(0xD0)
+        self.write_data(0x04)
+        self.write_data(0x0D)
+        self.write_data(0x11)
+        self.write_data(0x13)
+        self.write_data(0x2B)
+        self.write_data(0x3F)
+        self.write_data(0x54)
+        self.write_data(0x4C)
+        self.write_data(0x18)
+        self.write_data(0x0D)
+        self.write_data(0x0B)
+        self.write_data(0x1F)
+        self.write_data(0x23)
+
+        self.write_cmd(0xE1)
+        self.write_data(0xD0)
+        self.write_data(0x04)
+        self.write_data(0x0C)
+        self.write_data(0x11)
+        self.write_data(0x13)
+        self.write_data(0x2C)
+        self.write_data(0x3F)
+        self.write_data(0x44)
+        self.write_data(0x51)
+        self.write_data(0x2F)
+        self.write_data(0x1F)
+        self.write_data(0x1F)
+        self.write_data(0x20)
+        self.write_data(0x23)
+        
+        self.write_cmd(0x21)
+
+        self.write_cmd(0x11)
+
+        self.write_cmd(0x29)
+
+    def show(self):
+        self.write_cmd(0x2A)
+        self.write_data(0x00)
+        self.write_data(0x28)
+        self.write_data(0x01)
+        self.write_data(0x17)
+        
+        self.write_cmd(0x2B)
+        self.write_data(0x00)
+        self.write_data(0x35)
+        self.write_data(0x00)
+        self.write_data(0xBB)
+        
+        self.write_cmd(0x2C)
+        
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(self.buffer)
+        self.cs(1)
+        
+if __name__=='__main__':
+    lcd = ST7789()
+    lcd.fill(0xFFFF)
     lcd.show()
+    lcd.text("Raspberry Pi Pico",10,5,0xFF00)
+    lcd.text("PicoGo",10,15)
+    lcd.text("Waveshare.com",10,25,0x07E0)
+    lcd.show()
+    
+   
+
 
